4802d86990f14a08ca0ca9cc68a34107efd7770c
marnysan111 <o-masato@outlook.jp>
2022-08-15 14:28:05 +0900

    add: test page

---
+++ react/app/src/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from "react-router-dom"
+
+const Home = () => {
+    return (
+        <div>
+        <Link to="top">Top</Link>
+
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ react/app/src/pages/top.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from "react-router-dom"
+const Top = () => {
+    return (
+        <div>
+            <h1>PageA</h1>
+            <Link to="/">/</Link>
+        </div>
+    )
+}
+
+export default Top;
